16e25cea79de7fe5e476a0e0ea676d1f4f9c434b
Anik <sheikhanikbd@gmail.com>
2022-10-17 11:55:27 +0600

    const added

--- app/listeners/commands/out_command.py
+++ app/listeners/commands/out_command.py
@@ -8,6 +8,7 @@ from service_account import wks
 
 from app.blocks.you_are_out_message import you_are_out
 from app.util.date_time import now_date_str, now_time_str
+from app.util.const import date_col, name_col, slack_id_col, working_on_col, in_time_col, out_time_col, total_hour_col
 
 
 def out_command(ack: Ack, client: WebClient, body: dict, logger: Logger):
@@ -23,12 +24,12 @@ def out_command(ack: Ack, client: WebClient, body: dict, logger: Logger):
         data = wks.get_all_values()
 
         for i in range(len(data)):
-            if data[i][0] == now_date_str() and data[i][2] == user_id and data[i][5] == '':
+            if data[i][date_col] == now_date_str() and data[i][slack_id_col] == user_id and data[i][out_time_col] == '':
 
                 wks.update(f'F{i+1}', now_time_str(), raw=False)
 
-                time_out = datetime.strptime(data[i][4], '%I:%M %p')
-                difference = datetime.now() - time_out
+                time_in = datetime.strptime(data[i][in_time_col], '%I:%M %p')
+                difference = datetime.now() - time_in
                 hours, remainder = divmod(difference.seconds, 3600)
                 minutes, seconds = divmod(remainder, 60)
 
@@ -39,9 +40,9 @@ def out_command(ack: Ack, client: WebClient, body: dict, logger: Logger):
                 client.chat_postMessage(
                     channel="#soft-bot",
                     blocks=you_are_out(
-                        name=data[i][1], 
+                        name=data[i][name_col], 
                         date=now_date_str(),
-                        task=data[i][3], 
+                        task=data[i][working_on_col], 
                         time=now_time_str(),
                         working= total_hour,
                     )
---
+++ app/util/const.py
@@ -0,0 +1,7 @@
+date_col = 0
+name_col = 1
+slack_id_col = 2
+working_on_col = 3
+in_time_col = 4
+out_time_col = 5
+total_hour_col = 6
